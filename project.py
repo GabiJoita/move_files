188870cbf083110c3bf2bfa5ccfaac5afc4a8b55
GabiJoita <GabiJoita191@gmail.com>
2024-02-04 19:39:46 +0200

    project.py added

---
+++ project.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import shutil
+
+
+def create_directory(folder1, folder2):
+    """create 2 folders to put your files inside"""
+    try:
+        os.mkdir(folder1)
+        os.mkdir(folder2)
+        return folder1, folder2
+    except FileExistsError:
+        print('dir already exist')
+
+def create_files(file):
+    """return 2 files in format: txt, csv or xlsx"""
+    pass
+
+def open_files(file:str):
+    """open the 2 files created"""
+    pass
+
+def compare_files(file:str):
+    """compare the 2 files content """
+    pass
+
+
+def move_file(file:str):
+    """move the content from file1 to file2"""
+    pass
+
+
+if __name__ == "__main__":
+    path = input('enter your path: ')
+    dir1 = os.path.join(path, 'dir1')
+    dir2 = os.path.join(path, 'dir2')
+    print(create_directory(dir1, dir2))
